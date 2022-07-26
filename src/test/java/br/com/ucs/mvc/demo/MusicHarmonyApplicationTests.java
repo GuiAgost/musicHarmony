74ddd658ee164c7189e3faf844e2a2bbfc424362
Guilherme Agostini <gui.agostini@hotmail.com>
2022-07-25 21:11:23 -0300

    Inicio do projeto musicHarmony do TCC II

---
+++ src/test/java/br/com/ucs/mvc/demo/MusicHarmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucs.mvc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicHarmonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
