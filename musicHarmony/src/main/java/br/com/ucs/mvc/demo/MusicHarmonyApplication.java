0530f979c023580c42345434a444c6c91c490c81
Guilherme Agostini <gui.agostini@hotmail.com>
2022-07-25 21:02:52 -0300

    Inicio de projeto TCC II desenvovlvimento web

---
+++ musicHarmony/src/main/java/br/com/ucs/mvc/demo/MusicHarmonyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ucs.mvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicHarmonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicHarmonyApplication.class, args);
+	}
+
+}
