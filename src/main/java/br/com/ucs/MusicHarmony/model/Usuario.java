5e655b2830a09ce41b27a8bb655bf21410f19fd4
Guilherme Agostini <gui.agostini@hotmail.com>
2022-07-27 22:53:55 -0300

    Excluído pasta mvc

---
+++ src/main/java/br/com/ucs/MusicHarmony/model/Usuario.java
@@ -0,0 +1,19 @@
+package br.com.ucs.MusicHarmony.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+// import org.springframework.data.annotation.Id;
+// import javax.persistence.GeneratedValue;
+
+// Detalhes sobre lombok: https://medium.com/collabcode/projeto-lombok-escrevendo-menos-c%C3%B3digo-em-java-8fc87b379209
+
+@Getter@Setter@AllArgsConstructor
+public class Usuario {
+
+    // @Id @GeneratedValue
+    private int id;
+    private String senha;
+    private String nome;
+    private String email;
+}
---
+++ src/test/java/br/com/ucs/MusicHarmony/AppMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucs.MusicHarmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
