33623a195391881e0068ce53c412865d0eb3b202
Guilherme Agostini <gui.agostini@hotmail.com>
2022-09-15 21:14:00 -0300

    Imolementado a regra de negócio para postar o resultado.

--- src/main/java/br/com/ucs/MusicHarmony/MusicHarmonyApplication.java
+++ src/main/java/br/com/ucs/MusicHarmony/MusicHarmonyApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EntityScan(basePackages = "br.com.ucs.MusicHarmony.model")
-@SpringBootApplication
+@SpringBootApplication // É responsável por dispor diversas funcionalidades, sem que para isso tenhamos que fazer qualquer configuração
 public class MusicHarmonyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MusicHarmonyApplication .class, args);
 	}
-
 }
--- src/main/java/br/com/ucs/MusicHarmony/controller/QueriesController.java
+++ src/main/java/br/com/ucs/MusicHarmony/controller/QueriesController.java
@@ -5,7 +5,9 @@ import br.com.ucs.MusicHarmony.service.LogoutService;
 import br.com.ucs.MusicHarmony.service.TetradService;
 import br.com.ucs.MusicHarmony.service.TriadService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
@@ -15,11 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 public class QueriesController {
 
     @GetMapping("triade")
-    public String triad(HttpServletRequest request){
+    public String triad(HttpServletRequest request, Model model){
 
-        String resultTriad;
+        String answerTriade;
         String chord;
-        TriadService result = new TriadService();
+        TriadService answer = new TriadService();
 
         Boolean logged = getLogged(request);
         if (logged){
@@ -29,11 +31,18 @@ public class QueriesController {
             if(request.getParameter("chord") != null) {
                 chord = request.getParameter("chord");
                 System.out.println("Acorde informado: " + chord);
-                resultTriad = result.chordTriad(chord);
-                System.out.println(resultTriad);
+                answerTriade = answer.chordTriad(chord);
+                triadResult(answerTriade, model);
             }
             return "consultas/triade";
         }
+    }
+
+    @PostMapping("/triade")
+    public String triadResult(@ModelAttribute String resultTriad, Model model){
+        System.out.println(model.addAttribute("resultTriad", resultTriad));
+        model.addAttribute("resultTriad", resultTriad);
+        return "consultas/triade";
     }
 
     @GetMapping("tetrade")
