ba46cbb63f5f146e45e724e82895af61f74cab9a
Guilherme Agostini <gui.agostini@hotmail.com>
2022-09-22 22:26:46 -0300

    Adicionado classe modelo "ChordImage" para criação de tabela de imagem.

---
+++ src/main/java/br/com/ucs/MusicHarmony/model/ChordImage.java
@@ -0,0 +1,40 @@
+package br.com.ucs.MusicHarmony.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
+public class ChordImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="chordName", nullable=false, unique=true)
+    private String chordName;
+    @Column(name="image", nullable=false)
+    private byte[] image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getChordName() {
+        return chordName;
+    }
+
+    public void setChordName(String chordName) {
+        this.chordName = chordName;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
