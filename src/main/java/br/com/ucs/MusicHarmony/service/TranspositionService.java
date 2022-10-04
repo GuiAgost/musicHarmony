325945fe788c7449b972822565cd4de2b75d04a3
Guilherme Agostini <gui.agostini@hotmail.com>
2022-10-04 20:03:01 -0300

    Refatorado a classe service TranspositionService.

--- src/main/java/br/com/ucs/MusicHarmony/service/TranspositionService.java
+++ src/main/java/br/com/ucs/MusicHarmony/service/TranspositionService.java
@@ -1,33 +1,40 @@
 package br.com.ucs.MusicHarmony.service;
 
 import java.util.LinkedList;
+import static java.lang.Math.abs;
 
 public class TranspositionService {
 
     public String transposition(int semitone, String note){
-        final LinkedList<String> scale = new LinkedList<String>(){{add("C");add("C#");add("D");add("D#");add("E");add("F");add("F#");add("G");add("G#");add("A");add("A#");add("B");}};
+        final LinkedList<String> scale = new LinkedList<>(){{add("C");add("C#");add("D");add("D#");add("E");add("F");add("F#");add("G");add("G#");add("A");add("A#");add("B");}};
 
-//        if (note.matches("[0-9]*")) {
-//
-//        }
+        int aux = getAux(note, scale, 0);
+        int transposed = getTransposed(semitone, scale, aux);
+
+        System.out.println(scale.get(transposed));
+        return scale.get(transposed);
+    }
+    // https://en.wikipedia.org/wiki/Transposition_(music)
+
+    private int getTransposed(int semitone, LinkedList<String> scale, int aux) {
         int transposed;
-        int aux = 0;
-
-        for(int i = 0; i < scale.size(); i++){
-            if(scale.get(i).equals(note)){
-                aux = i;
-            }
-        }
         int sum = aux + semitone;
 
         if (sum >= 0) {
             transposed = sum % scale.size();
         } else {
-            transposed = Math.abs(scale.size() + sum) % scale.size();
+            transposed = abs(scale.size() + sum) % scale.size();
         }
+        return transposed;
+    }
 
-        System.out.println(scale.get(transposed));
-        return scale.get(transposed);
+    private int getAux(String note, LinkedList<String> scale, int aux) {
+        for(int i = 0; i < scale.size(); i++){
+            if(scale.get(i).equals(note)){
+                aux = i;
+            }
+        }
+        return aux;
     }
 }
 
