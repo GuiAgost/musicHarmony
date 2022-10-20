78a83702cf1332f28f2aef683c4e25dd2d6a1fce
Guilherme Agostini <gui.agostini@hotmail.com>
2022-10-20 18:05:28 -0300

    Implementado as validações de notas/acordes para transposição.

--- src/main/java/br/com/ucs/MusicHarmony/service/ValidationChordService.java
+++ src/main/java/br/com/ucs/MusicHarmony/service/ValidationChordService.java
@@ -7,7 +7,6 @@ public class ValidationChordService {
 
     public boolean validation(String chordNote){
 
-
         if (chordNote.matches("[H-Z]") || chordNote.matches("[0-9]*") || chordNote.matches("[a-z]")) {
             System.out.println("Passou a validação das letras inválidas e numeros inválidos: " + chordNote.matches("[H-Z]"));
             return false;
